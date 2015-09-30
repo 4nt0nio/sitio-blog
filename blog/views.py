d382665beb5014bb3ad131c6a3e498f72a38da6f
Antonio De Leon <jtoni91@gmail.com>
2015-09-29 21:27:05 -0600

    Cuarto commit, agregando los post y css a la pagina

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def lista_posts(request):
-	return render(request, 'blog/lista_posts.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now())
+	return render(request, 'blog/lista_posts.html', {'posts': posts})
