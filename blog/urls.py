ef177c7b99a40df3eeb061876b63c98724d98a6e
Antonio De Leon <jtoni91@gmail.com>
2015-10-09 10:44:09 -0600

    se agregaron formularios para editar posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.lista_posts),
+    url (r'^post/(?P<pk>[0-9]+)/$', views.detalle_post),
+    url(r'^post/nuevo/$', views.nuevo_post, name='nuevo_post'),
+    url(r'^post/(?P<pk>[0-9]+)/editar/$', views.editar_post, name='editar_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,39 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def lista_posts(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now())
 	return render(request, 'blog/lista_posts.html', {'posts': posts})
+
+def detalle_post(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/detalle_post.html', {'post': post})
+
+def nuevo_post(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.save()
+			return redirect('blog.views.detalle_post', pk=post.pk)
+	else:
+		form = PostForm()
+	return render(request, 'blog/editar_post.html', {'form': form})
+
+def editar_post(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == "POST":
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.save()
+			return redirect('blog.views.detalle_post', pk=post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(request, 'blog/editar_post.html', {'form': form})
